fe32fa8576e1d519e8683db73550d192fb24e8b4
biork23 <ismaelbiork@gmail.com>
2021-11-24 19:09:13 -0600

    add first class

--- sset/js/js/base.js
+++ sset/js/js/base.js
@@ -1,1 +1,22 @@
-console.groupCollapsed("hola mundo")
+
+const a = parseInt(prompt(" dame el valor de A"))
+const b = parseInt(prompt("dame el valor de B"))
+console.log(isNaN(a), isNaN(b))
+
+if(isNaN(a) === false && isNaN(b) === false){
+    if(a>b){
+        console.log(a/b)
+    } else if(a<b){
+        console.log(a+b)
+    } else if(a === b){
+        console.log(a * b)
+    }
+} else {
+    console.error('algunos de los caracteres no son numeros')
+}
+
+for(i = 1; ,= 10; i++)
+    console.log(`tabla del ${1*3}`)
+    for(j = 1; j ,= 10 ; j++) {
+        console.log(`${i} x ${j} = ${ i * j }`)
+    }
