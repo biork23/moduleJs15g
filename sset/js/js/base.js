3ae2a6d8afbaf064537f2c7408aed56514cb82dc
biork23 <ismaelbiork@gmail.com>
2021-11-30 19:49:41 -0600

    class-five activities

--- sset/js/js/base.js
+++ sset/js/js/base.js
@@ -1,179 +1,109 @@
 
 
+console.log('funciones')
 
-let saludo = 'hola' 
- for(i = 0; i < saludo.length; i++ ) {
-     console.log(saludo[i])
+function suma(){
+    console.log('2 + 2')
+    
 }
 
-//  pedir al usuario un string
-//  contar las vocales y las consonantes
-// imprimir en consola
-// 2 vocales y 2 consonantes
+suma()
 
-const userWord = prompt('Dame una palabra')
- let vowels = 0
- let consonant = 0
+//funcion con parametros
+function suma(a, b) {
+    console.log(a + b)
+}
 
- if(userWord.length > 0){
-    
-     for(i = 0; i < userWord.length; i++ ) {
-         let letter = userWord[i].toLowerCase()
-         if(letter !== ' '){
-             if(
-                 letter === 'a' ||
-                 letter === 'e' ||
-                 letter === 'i' ||
-                 letter === 'o' ||
-                 letter === 'u' ||
-                 letter === 'á' ||
-                 letter === 'é' ||
-                 letter === 'í' ||
-                 letter === 'ó' ||
-                 letter === 'ú'
-             ) {
-                 //vowels = vowels + 1
-                 //vowels += 1
-                 vowels++
-             } else {
-                 consonant++
-             }
-         }
-     }
-    
-     console.log(`${vowels} vocales y ${consonant} consonantes`)
- } else {
-     console.log('Inserta una palabra por favor')
- }
+suma (9, 3)
+
+function toCelcius(farenheit){
+    console.log( (5 / 9) * (farenheit - 32) )
+} 
+ toCelcius(100)
+
+//funcion que pida el nombre al ususario
+// y lo imprima en cosola
+
+/**
+ * Funcion que reciba una palabra 
+ * y la retorne al reves
+ */
+
+function askName (firstName) {
+    console.log(firstName)
+}
+
+askName(prompt('Escribe tu nombre'))
+
+function reverseWord (word){
+    let wordReversed = ''
+    for(j = word.length - 1; j >= 0 ; j--){
+        wordReversed  += wordReversed[j]
+    } 
+    return wordReversed   
+}
+let nombreAlReves = reverseWord
+console.log(nombreAlReves)
 
 
-
- //pedir una palanbra con espacio al inicio y al final
- //contar las letras "a" y "e", 
- //Contar numero de espacios, sin contar los espacios
- //al inicio y al final
-
- const userWord = prompt('Dame una palabra').toLowerCase().trim()
- console.log(userWord)
- let letterA = 0
- let letterE = 0
- let spaces = 0
-
- if(userWord.length > 0){
-     for(i = 0; i < userWord.length; i++ ) {
-         let letter = userWord[i].toLowerCase()
-         if(letter !== ' '){
-             if(letter === 'a' || letter === 'á') {
-                 letterA++
-             } else if(letter === 'e' || letter === 'é') {
-                 letterE++
-             }
-         } else {
-             spaces++
-         }
-     }
-     console.log(`${letterA} "A" y ${letterE} "E"`)
- } else {
-     console.log('Inserta una palabra por favor') }
-
-
-// ciclos
-
-// inicio
-// fin
-// incremento
-
- for( i= 1; i <= 10; i++){
-     console.log(`5 x ${i} = ${5 * i}`)
- }
-
-// Pedir el usuario 1 numero entre 1 y 100
-// sumar todos los numeros entre el 1 y este numero
-// Mandar un alert con el  total
-
-// 10
-// 1+2+3+4+5+6+7+8+9+10
-// total -> ?
- const numberUser = parseInt(prompt('Dame un numero entre 1 y 100'))
- let suma = 0
- if(!isNaN(numberUser)){
-    
-     for( i = 1; i <= numberUser; i++){
-         suma += i
-     }
-     alert(`La suma es: ${suma}`)
- } else {
-     console.log('No es un numero valido')
- }
-
-
-
-// inicio
-// fin
-// incremento
-
-// while(condition){
-//     // sentences
-// }
-
- const numberUser = parseInt(prompt('Dame un numero entre 1 y 100'))
- let suma = 0
- let counter = 1
-
- if(!isNaN(numberUser) && numberUser > 0 &&  numberUser <= 100){  
-     while(counter <= numberUser){
-         // sentencias
-         suma = suma + counter
-
-         // incremento
-         counter = counter + 1
-
-     }
-    
-     alert(`La suma es: ${suma}`)
-
- } else {
-     console.error('No es un numero valido')
- }
-
- //do {
-     // sentences
-// } while(condition)
-
-
-// pedir al usuario una palabra (permitir espacios, mayusculas)
-// Verificar que sea un palindromo
-// mandar en consola el resultado
-// -> La palabra "anita lava la tina" es un palindromo
-
-
-
-// For solution
-const wordUser = prompt('Dame una palabra ').trim().toLowerCase()
-
-let wordWithoutSpace = ''
-let wordReversed = ''
-
-if(wordUser != ''){
-    // Remove White space
-    for(i = 0; i < wordUser.length; i++){
-        if(wordUser[i] !== ' '){
-            wordWithoutSpace = wordWithoutSpace +  wordUser[i]
+//Función 1:
+//Imprimir en consola las tablas de multiplicar del 1 al 10 
+function tables(){
+    for(i = 1; i <= 10; i++){
+        for(j = 1; j <= 10; j++){
+            console.log(`${i} x ${j} = ${i * j}`)
         }
     }
-    console.log(wordWithoutSpace)
-
-    // word reversed  
-    for(j = wordWithoutSpace.length - 1; j >= 0 ; j--){
-        wordReversed = wordReversed + wordWithoutSpace[j]
-    } 
-    console.log(wordReversed)
-
-    wordWithoutSpace === wordReversed ?
-        console.log(`La palabra "${wordUser}" es un palindromo`)
-    :
-        console.log(`La palabra "${wordUser}" No es un palindromo`)
-
-} else {
-    console.log('Inserta una palabra valida')
 }
+tables()
+//Función 2:
+//Imprimir en consola la suma de 1 a N, siendo N un numero entre 1 y 100
+
+
+//Función 3:
+//Imprimir en consola si una oración es un palindromo (puede contener espacios)
+//anita lava la tina -> True
+
+function checkPalindrome(string) {
+
+    const arrayValues = string.split('');
+
+    const reverseArrayValues = arrayValues.reverse();
+
+    const reverseString = reverseArrayValues.join('');
+
+    if(string == reverseString) {
+        console.log('Es palindromo');
+    }
+    else {
+        console.log('No es palindromo');
+    }
+}
+
+const string = prompt('Ingresa una string: ');
+
+checkPalindrome(string);
+
+//Función 4:
+//Imprimir en consola la suma total de todos los digitos de una cantidad 
+//p.ej. "1234" -> 10
+
+// Función 5:
+//Imprimir en consola la suma de los múltiplos de 3 y 5 contenidos entre el 1 y 100
+//-> 233168*/
+function factors() {
+    let counterMulti = 0
+    for(i = 1; i <= 100; i++){
+        i % 3 === 0 || i % 5 === 0 ? counterMulti += i : ''
+    }   
+}
+factors()
+
+
+//ejercicio
+
+function sumNumbers(a, b = 5){
+    let suma = a + b
+    return suma
+}
+let resultado = sumNumbers(4)
--- sset/js/js/homework.js
+++ sset/js/js/homework.js
@@ -1,47 +1,37 @@
-// pedir al usuario una palabra (permitir espacios, mayusculas)
-// Verificar que sea un palindromo
-// mandar en consola el resultado
-// -> La palabra "anita lava la tina" es un palindromo
-
-
-
-// For solution
-const wordUser = prompt('Dame una palabra ').trim().toLowerCase()
-
-let wordWithoutSpace = ''
-let wordReversed = ''
-
-if(wordUser != ''){
-    // Remove White space
-    for(i = 0; i < wordUser.length; i++){
-        if(wordUser[i] !== ' '){
-            wordWithoutSpace = wordWithoutSpace +  wordUser[i]
+// Función 1:
+// valor por defecto 10
+// Imprimir en consola las tablas de multiplicar del 1 al 10 
+function tables(a=10){
+    for(i = 1; i <= 10; i++){
+        for(j = 1; j <= 10; j++){
+            console.log(`${i} x ${j} = ${i * j}`)
         }
     }
-    console.log(wordWithoutSpace)
-
-    // word reversed  
-    for(j = wordWithoutSpace.length - 1; j >= 0 ; j--){
-        wordReversed = wordReversed + wordWithoutSpace[j]
-    } 
-    console.log(wordReversed)
-
-    wordWithoutSpace === wordReversed ?
-        console.log(`La palabra "${wordUser}" es un palindromo`)
-    :
-        console.log(`La palabra "${wordUser}" No es un palindromo`)
-
-} else {
-    console.log('Inserta una palabra valida')
 }
+tables(10)
+
+// Función 2:
+// valor por defecto 10
+// Imprimir en consola la suma de 1 a N, siendo N un numero entre 1 y 100
+
+// Función 3:
+// valor por defecto "1234"
+// Imprimir en consola la suma total de todos los digitos de una cantidad 
+// p.ej. "1234" -> 10
 
 
-// Homework
-// De la solucion anterior generar lo siguiente
-// 1. While solution
-// 2. Permitir acentos
-// 
 
+// Función 4:
+// valor por defecto 3 y 5, sino el que ponga el usuario 
+// Imprimir en consola la suma de los múltiplos de 3 y 5 contenidos entre el 1 y 100
+// -> 233168
 
-// Imprimir en consola la suma de los multiplos de 3 y 5 contenidos entre el 1 y 100
-// -> 233168git status
+function factors(a=3, b=5) {
+    let counterMulti = 0
+    for(i = 1; i <= 100; i++){
+        i % 3 === 0 || i % 5 === 0 ? counterMulti += i : ''
+    }   
+}
+factors(3, 5)
+
+// invertigar Arrow functions
