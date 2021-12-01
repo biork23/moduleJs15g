bf11ed6a0514f884d97a83b7dced129cd8719603
biork23 <ismaelbiork@gmail.com>
2021-11-30 23:15:10 -0600

    actividad class-five

--- sset/js/js/base.js
+++ sset/js/js/base.js
@@ -1,25 +1,25 @@
 
 
-console.log('funciones')
+// console.log('funciones')
 
-function suma(){
-    console.log('2 + 2')
+// function suma(){
+//     console.log('2 + 2')
     
-}
+// }
 
-suma()
+// suma()
 
-//funcion con parametros
-function suma(a, b) {
-    console.log(a + b)
-}
+// //funcion con parametros
+// function suma(a, b) {
+//     console.log(a + b)
+// }
 
-suma (9, 3)
+// suma (9, 3)
 
-function toCelcius(farenheit){
-    console.log( (5 / 9) * (farenheit - 32) )
-} 
- toCelcius(100)
+// function toCelcius(farenheit){
+//     console.log( (5 / 9) * (farenheit - 32) )
+// } 
+//  toCelcius(100)
 
 //funcion que pida el nombre al ususario
 // y lo imprima en cosola
@@ -29,81 +29,106 @@ function toCelcius(farenheit){
  * y la retorne al reves
  */
 
-function askName (firstName) {
-    console.log(firstName)
-}
+// function askName (firstName) {
+//     console.log(firstName)
+// }
 
-askName(prompt('Escribe tu nombre'))
+// askName(prompt('Escribe tu nombre'))
 
-function reverseWord (word){
-    let wordReversed = ''
-    for(j = word.length - 1; j >= 0 ; j--){
-        wordReversed  += wordReversed[j]
-    } 
-    return wordReversed   
-}
-let nombreAlReves = reverseWord
-console.log(nombreAlReves)
+// function reverseWord (word){
+//     let wordReversed = ''
+//     for(j = word.length - 1; j >= 0 ; j--){
+//         wordReversed  += wordReversed[j]
+//     } 
+//     return wordReversed   
+// }
+// let nombreAlReves = reverseWord
+// console.log(nombreAlReves)
 
 
 //Función 1:
 //Imprimir en consola las tablas de multiplicar del 1 al 10 
-function tables(){
-    for(i = 1; i <= 10; i++){
-        for(j = 1; j <= 10; j++){
-            console.log(`${i} x ${j} = ${i * j}`)
-        }
-    }
-}
-tables()
+ function tables(){
+     for(i = 1; i <= 10; i++){
+         for(j = 1; j <= 10; j++){
+             console.log(`${i} x ${j} = ${i * j}`)
+         }
+     }
+     return tables
+ }
+ tables()
 //Función 2:
 //Imprimir en consola la suma de 1 a N, siendo N un numero entre 1 y 100
+function addNumbersRange() {
+    let addRange = 0
+    if(!isNaN(parseInt(numberEnd)) || numberEnd >= 1 && numberEnd <= 100){
+        for(i = 1; i <= numberEnd; i++){
+            addRange += i
+        }
+    } else {
+        console.log('No es un número valido')
+    }
 
+    return addRange
+}
 
 //Función 3:
 //Imprimir en consola si una oración es un palindromo (puede contener espacios)
 //anita lava la tina -> True
 
-function checkPalindrome(string) {
+ function checkPalindrome(string) {
 
-    const arrayValues = string.split('');
+     const arrayValues = string.split('');
 
-    const reverseArrayValues = arrayValues.reverse();
+     const reverseArrayValues = arrayValues.reverse();
 
-    const reverseString = reverseArrayValues.join('');
+     const reverseString = reverseArrayValues.join('');
 
-    if(string == reverseString) {
-        console.log('Es palindromo');
-    }
-    else {
-        console.log('No es palindromo');
-    }
-}
+     if(string == reverseString) {
+         console.log('Es palindromo');
+     }
+     else {
+         console.log('No es palindromo');
+     }
+ }
 
-const string = prompt('Ingresa una string: ');
+ const string = prompt('Ingresa una string: ');
 
-checkPalindrome(string);
+ checkPalindrome(string);
 
 //Función 4:
 //Imprimir en consola la suma total de todos los digitos de una cantidad 
 //p.ej. "1234" -> 10
+function additionNumbers () {
+    let numberToAdd = numberValue.toString()
+    let addNnumber = 0
+    for(i = 0; i < numberToAdd.length; i++){
+        addNnumber += parseInt(numberToAdd[i])
+    }
+
+    return addNnumber
+}
+
 
 // Función 5:
 //Imprimir en consola la suma de los múltiplos de 3 y 5 contenidos entre el 1 y 100
 //-> 233168*/
-function factors() {
-    let counterMulti = 0
-    for(i = 1; i <= 100; i++){
-        i % 3 === 0 || i % 5 === 0 ? counterMulti += i : ''
-    }   
-}
-factors()
+ function factors() {
+         let counterMulti = 0
+     for(i = 1; i <= 100; i++){
+             i % 3 === 0 || i % 5 === 0 ? counterMulti += i : ''
+     }   
+ }
+ factors()
 
 
 //ejercicio
 
-function sumNumbers(a, b = 5){
-    let suma = a + b
-    return suma
-}
-let resultado = sumNumbers(4)
+// function sumNumbers(a, b = 5){
+//     let suma = a + b
+//     return suma
+// }
+// let resultado = sumNumbers(4)
+
+
+
--- sset/js/js/homework.js
+++ sset/js/js/homework.js
@@ -1,12 +1,13 @@
 // Función 1:
 // valor por defecto 10
 // Imprimir en consola las tablas de multiplicar del 1 al 10 
-function tables(a=10){
+function tables(j = 10){
     for(i = 1; i <= 10; i++){
         for(j = 1; j <= 10; j++){
             console.log(`${i} x ${j} = ${i * j}`)
         }
     }
+    return tables
 }
 tables(10)
 
@@ -14,10 +15,35 @@ tables(10)
 // valor por defecto 10
 // Imprimir en consola la suma de 1 a N, siendo N un numero entre 1 y 100
 
+function addNumbersRange(numberEnd = 10) {
+    let addRange = 0
+    if(!isNaN(parseInt(numberEnd)) || numberEnd >= 1 && numberEnd <= 100){
+        for(i = 1; i <= numberEnd; i++){
+            addRange += i
+        }
+    } else {
+        console.log('No es un número valido')
+    }
+
+    return addRange
+}
+
+
+
 // Función 3:
 // valor por defecto "1234"
 // Imprimir en consola la suma total de todos los digitos de una cantidad 
 // p.ej. "1234" -> 10
+
+function additionNumbers (numberValue = 1234) {
+    let numberToAdd = numberValue.toString()
+    let addNnumber = 0
+    for(i = 0; i < numberToAdd.length; i++){
+        addNnumber += parseInt(numberToAdd[i])
+    }
+
+    return addNnumber
+}
 
 
 
@@ -26,12 +52,20 @@ tables(10)
 // Imprimir en consola la suma de los múltiplos de 3 y 5 contenidos entre el 1 y 100
 // -> 233168
 
-function factors(a=3, b=5) {
-    let counterMulti = 0
-    for(i = 1; i <= 100; i++){
-        i % 3 === 0 || i % 5 === 0 ? counterMulti += i : ''
-    }   
+function getMulti (numberMulti = 'default') {
+    let addMulti = 0
+    if(numberMulti === 'default') {
+        for(i = 0; i <= 100; i++){
+            i % 3 === 0 || i % 5 === 0  ? addMulti += i : ''
+        }
+    } else {
+        for(i = 0; i <= 100; i++){
+            i % numberMulti === 0 ? addMulti += i : ''
+        }
+    }
+    
+    return addMulti
 }
-factors(3, 5)
+
 
 // invertigar Arrow functions
