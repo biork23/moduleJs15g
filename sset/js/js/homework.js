a67307955906ca2c5b6d8d78c2903702c68577d2
biork23 <ismaelbiork@gmail.com>
2021-12-01 23:19:46 -0600

    homwork 6

--- sset/js/js/homework.js
+++ sset/js/js/homework.js
@@ -53,9 +53,94 @@ const checkPalindrome =(string) => {
 */
 
 
+const arrayReduce = (arrOne, arrTwo) => {
+    let newArr = arrOne.concat(arrTwo)
+    let arrAdd = 0
+    
+    // funcion tradicional
+    // newArr.forEach( function (item) {
+    //     arrAdd += item
+    // })
+
+    // arrow function
+    newArr.forEach( value => {
+        arrAdd = arrAdd + value
+    })
+
+    // newArr.forEach( value => arrAdd += value )
+    return arrAdd
+}
+
+
+let resultado = arrayReduce( [1,2], [3] )
+console.log(resultado)
+
+
+
+// middle
+// const arrayReduce = (arrOne, arrTwo) => {
+//     let arrAdd =  0
+//     arrOne.concat(arrTwo).forEach( item => arrAdd += item )
+//     return arrAdd
+// }
+
+// short
+// const arrayReduce = (arrOne, arrTwo) => {
+//     let arrAdd =  0
+//     return arrOne.concat(arrTwo).forEach( item => arrAdd += item )
+// }
+
+// one line solution
+// const arrayReduce = (arrOne, arrTwo) => arrOne.concat(arrTwo).reduce( (acc, cv) => acc += cv )
+
 /**
  * Escribir una función que tome como parametro 2 arrays, y devuelva un array 
  * con los numeros repetidos en los 2 arrays
  * arrayReduce([1,2,3,4],[1,2,5]). 
  * -> [1,2]
 */
+
+// large
+// const getRepeatItems =  (arrOne, arrTwo) => {
+//     let concatArr = arrOne.concat(arrTwo)
+//     let arrRepeated = []
+//     concatArr.forEach((element, index) =>  {
+//         // console.log( concatArr.indexOf(element), index)
+//         if( concatArr.indexOf(element) !== index ) {
+//             arrRepeated.push(element)
+//         }
+//     })
+//     return arrRepeated
+// }
+
+// middle
+// const getRepeatItems =  (arrOne, arrTwo) => {
+//     let arrRepeated = []
+//     arrOne.concat(arrTwo).forEach((element, index) =>  {
+//         concatArr.indexOf(element) !== index ? arrRepeated.push(element) : ''
+//     })
+//     return arrRepeated
+// }
+
+
+// Large filter
+// const getRepeatItems =  (arrOne, arrTwo) => {
+//     let arrRepeated = []
+//     arrRepeated = arrOne.concat(arrTwo).filter((element, index, arr) =>  {
+//         if( arr.indexOf(element) !== index ) {
+//             return element
+//         }
+//     })
+
+//     return arrRepeated
+// }
+
+// short filter
+// const getRepeatItems = (arrOne, arrTwo) => {
+//     return arrOne.concat(arrTwo).filter((element, index,arr) =>  {
+//         return arr.indexOf(element) !== index ? element : ''
+//     })
+// }
+
+// one line -- not recomended
+const getRepeatItems = (arrOne, arrTwo) => arrOne.concat(arrTwo).filter((item, index,arr) => arr.indexOf(item) !== index ? item : '')
