ba2269e5295b6402bca36ae162aa8e1d428475fd
biork23 <ismaelbiork@gmail.com>
2021-12-20 18:25:50 -0600

    class18

--- updatepost.js
+++ updatepost.js
@@ -1,75 +1,15 @@
-// POST crear datos
-const createUser =  (objPost) => {
-    const xhttp = new XMLHttpRequest()
-    xhttp.open( "POST" , "https://genjs-292ac-default-rtdb.firebaseio.com/posts/.json", true)
-    xhttp.onload = function(data) {
-        if(data.target.status === 200){
-            document.querySelector('#title').value = ''
-            document.querySelector('#author').value = ''
-            document.querySelector('#timetoread').value = ''
-            document.querySelector('#resume').value = ''
-            let res = JSON.parse(data.target.response )
-            let idPost = res.name
-            document.getElementById('post__id').innerText = idPost
-            document.getElementById('alert__response').classList.remove('d-none')
-        }
-    }
-    xhttp.send( JSON.stringify(objPost) )
-}
-
-
-let send__post = document.querySelector('#send__post')
-send__post.addEventListener('click', () => {
-
-    let title = document.querySelector('#title').value
-    let author = document.querySelector('#author').value
-    let timetoread = document.querySelector('#timetoread').value
-    let resume = document.querySelector('#resume').value
-
-    
-    // console.log(title, author, timetoread, resume)
-    if(
-        title !== '' &&
-        author !== '' &&  
-        timetoread !== '' &&
-        resume !== ''
-    ){
-
-        let objNewPost = {
-            title: title,
-            author: author,
-            timetoread: timetoread,
-            resume: resume
-        }
-    
-        createUser(objNewPost)
-    } else {
-        alert('Algunos datos estan vacios')
-    }
-
-})
-
-
-
-// let objNewPost =  {
-//     author: 'Ismael Monfil',
-//     title: 'foo',
-//     body: 'bar lorem',
-
-// }
-
 
 // ir por los datos del post
 window.addEventListener('load', () => {
 
     const xhttp = new XMLHttpRequest()
     let idPost = location.search.slice(8)
-    console.log(idPost)
     xhttp.open( "GET" , `https://genjs-292ac-default-rtdb.firebaseio.com/posts/${idPost}.json`, true)
     xhttp.onload = function(data) {
         if(data.target.status === 200) {
             let objResp = JSON.parse(data.target.response)
-            console.log(objResp)
+            
+            // console.log(objResp)
             // Llenar el formulario
 
             document.querySelector('#title').value = objResp.title
@@ -124,18 +64,18 @@ update__post.addEventListener('click', () => {
 
 })
 
-//Delete eliminar post
+// DELETE eliminar post
 
 let delete__post = document.getElementById('delete__post')
 delete__post.addEventListener('click', () => {
     let idPost = location.search.slice(8)
     const xhttp = new XMLHttpRequest()
-    xhttp.open("DELETE", `https://genjs-292ac-default-rtdb.firebaseio.com/posts/${idPost}.json`, true)
-    xhttp.onload = funtion(data);{
+    xhttp.open( "DELETE" , `https://genjs-292ac-default-rtdb.firebaseio.com/posts/${idPost}.json`, true)
+    xhttp.onload = function(data) {
         if(data.target.status === 200){
-            //location.replace(http://127.0.0.1:5500/')
-            //document.getElementById('alert__response').classlist.remove('d-none')
+            location.replace('http://127.0.0.1:5500/')
+            // document.getElementById('alert__response').classList.remove('d-none')
         }
     }
-    xhttp.send()
+    xhttp.send() 
 })
